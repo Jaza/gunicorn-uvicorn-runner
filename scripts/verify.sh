ef7a7af4942d3142337fa7e7e68c18c0a77d1aef
Jeremy Epstein <jazepstein@gmail.com>
2022-10-11 16:46:29 +1100

    fix bandit

--- scripts/verify.sh
+++ scripts/verify.sh
@@ -5,6 +5,6 @@ $HOME/.local/bin/poetry run black gunicorn_uvicorn_runner tests --check
 $HOME/.local/bin/poetry run isort --check-only gunicorn_uvicorn_runner tests
 $HOME/.local/bin/poetry run flake8 gunicorn_uvicorn_runner tests
 $HOME/.local/bin/poetry run mypy gunicorn_uvicorn_runner tests
-$HOME/.local/bin/poetry run bandit -r gunicorn_uvicorn_runner tests
+$HOME/.local/bin/poetry run bandit -c pyproject.toml -r gunicorn_uvicorn_runner tests
 
 set +e
